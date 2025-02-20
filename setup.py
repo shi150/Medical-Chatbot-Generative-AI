9d9eb61fc3c22e3caa54bd897584071673dae956
Nagelli Shivani <shivaninagelli@gmail.com>
2025-02-20 12:55:39 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ='Generative AI Project',
+    version='0.0.0',
+    author='Nagelli Shivani',
+    author_email='shivaninagelli@gmail.com',
+    packages=find_packages(),
+    install_requies=[]
+)
